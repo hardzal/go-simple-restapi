36dea42119f35ec6530063028938b9803bf241a9
hardzal <suryadijogja@gmail.com>
2022-02-16 15:55:54 +0700

    add category service

---
+++ helper/model.go
@@ -0,0 +1,22 @@
+package helper
+
+import (
+	"restfulapi-golang/model/domain"
+	"restfulapi-golang/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
+	}
+
+	return categoryResponses
+}
---
+++ model/web/category_create_request.go
@@ -0,0 +1,5 @@
+package web
+
+type CategoryCreateRequestRequest struct {
+	Name string
+}
