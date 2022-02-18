4bc3ae34d895af70290a887a5c5dc23b3656225b
hardzal <suryadijogja@gmail.com>
2022-02-18 19:13:33 +0700

    add error handler and notifications

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -1,13 +1,63 @@
 package exception
 
 import (
+	"github.com/go-playground/validator/v10"
 	"net/http"
 	"restfulapi-golang/helper"
 	"restfulapi-golang/model/web"
 )
 
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
+
+	if notFoundError(writer, request, err) {
+		return
+	}
+
+	if validationErrors(writer, request, err) {
+		return
+	}
+
 	InternalServerError(writer, request, err)
+}
+
+func validationErrors(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+		return true
+	}
+
+	return false
+}
+
+func notFoundError(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(NotFoundError)
+
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusNotFound)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "NOT FOUND",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+		return true
+	}
+
+	return false
 }
 
 func InternalServerError(writer http.ResponseWriter, request *http.Request, err interface{}) {
